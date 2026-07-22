b1c522a76800ed63228c9535f60ae1b25f3069f5
Paolo <paolohagos08@gmail.com>
2026-07-22 09:39:20 +0800

    refactor: code cleanup

--- src/Shared/Dtos/ProxmoxHostDto.cs
+++ src/Shared/Dtos/ProxmoxHostDto.cs
@@ -1,12 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shared.Dtos;
 
 public class ProxmoxHostDto
 {
     public Guid Id { get; set; }
-    public required string HostName { get; set; }
-    public required string ServerUrl { get; set; }
-    public required string ApiToken { get; set; }
+    public string HostName { get; set; }
+    public string ServerUrl { get; set; }
+    public string ApiToken { get; set; }
     public bool IsActive { get; set; }
 }
